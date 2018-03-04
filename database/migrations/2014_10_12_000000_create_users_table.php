8c3dfbde83de9d1f62922a5077ef11521e59d9d5
uzzalkumar <uzzalroy.acm@gmail.com>
2018-03-05 00:31:08 +0600

    Create migrate prouects, reviews, users, reset_password

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,9 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('name', 64);
+            $table->string('email', 128)->unique();
+            $table->string('password', 32);
             $table->rememberToken();
             $table->timestamps();
         });
